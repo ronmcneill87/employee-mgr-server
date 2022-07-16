4b2683e9d852ac915ea3950c29a17d2586ebe591
Ronald McNeill <ronaldmcneill@ronalds-air.myfiosgateway.com>
2022-07-16 10:54:14 -0400

    add

---
+++ src/main/java/com/mcneill/employeemgrserver/EmployeeMgrServerApplication.java
@@ -0,0 +1,13 @@
+package com.mcneill.employeemgrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgrServerApplication.class, args);
+	}
+
+}
